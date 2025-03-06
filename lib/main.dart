72c4fd79888bbcc97886a2d01b9d46badfefd20f
mohamed-ahmed-othman <ma7080642@gmail.com>
2025-03-06 02:16:01 +0200

    added spalsh screen for ios & android below 12 added spash screen for android 12

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:oop/core/routing/app_router.dart';
 import 'package:oop/doc_app.dart';
 
 void main() {
+  //update the runApp function to use the DocApp widget
   runApp(
     DocApp(
       appRouter: AppRouter(),
